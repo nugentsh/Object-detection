1d2c2dcdf708de6734d76304278f8da5c292ac85
nugentsh <45015718+nugentsh@users.noreply.github.com>
2019-03-18 16:09:17 +0000

    Add files via upload

---
+++ myTrigger.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as gpi
+import pygame as pg
+from object_detection import object_detection
+from object_detection import desired_detect
+
+
+pg.mixer.init()
+pg.mixer.music.load("") #file name here
+
+
+
+gpi.setwarnings(False)
+gpi.setmode(gpi.BCM)
+
+gpi.setup(36, gpi.IN) #GPIO-16 INPUT-Button
+
+gpi.setup(31, gpi.OUT) #GPIO-6 OUTPUT-Speaker
+
+# evaluate if button is pressed
+if gpi.input(36):
+    print("button triggered")
+    object_detection()
+    if desired_detect == True:
+        gpi.output(31,True)
+        pg.mixer.music.play() #play sound
+
+    else:
+        print("No person detected")

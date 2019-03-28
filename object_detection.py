c10b5ab8b8857f9b4a61e32a74557ae782174e35
nugentsh <45015718+nugentsh@users.noreply.github.com>
2019-03-28 19:51:57 +0000

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -1,24 +1,24 @@
 import numpy as np
 import cv2
-from picamera import PiCamera
-from time import sleep
+#from picamera import PiCamera
+#from time import sleep
 
 # take picture with camera module and save as 'image.jpg'
-PiCamera().start_preview()
-sleep(5)
-PiCamera().capture('image.jpg')
-PiCamera().stop_preview()
+#PiCamera().start_preview()
+#sleep(5)
+#PiCamera().capture('image.jpg')
+#PiCamera().stop_preview()
 
 desired_detect = False
 
 # initialize the list of class labels MobileNet SSD was trained to detect
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-	"sofa", "train", "tvmonitor"]
-	
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor"]
+
 # then generate a random set of bounding box colors for each class
-COLORS = np.random.uniform(0, 255, size = (len(CLASSES), 3))
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 # load the MobileNet pre-trained model
 print("[INFO] loading model...")
@@ -27,7 +27,7 @@ net = cv2.dnn.readNetFromCaffe('MobileNetSSD_deploy.prototxt.txt', 'MobileNetSSD
 # load the input image
 # resize to 300x300 pixels and then normalize
 # normalization is done by MobileNet SSD
-image = cv2.imread('image.jpg')
+image = cv2.imread('im3.jpeg')
 (h, w) = image.shape[:2]
 blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
 
@@ -36,43 +36,53 @@ print("[INFO] computing object detections...")
 net.setInput(blob)
 detections = net.forward()
 
-# set threshhold for detections to %20 confidence
-threshhold = 0.2 
+# set threshhold for detections to 50% confidence
+threshhold = 0.5
 
 # loop over the detections
 for i in np.arange(0, detections.shape[2]):
-	# extract the confidence in the prediction
-	confidence = detections[0, 0, i, 2]
+    # extract the confidence in the prediction
+    confidence = detections[0, 0, i, 2]
 
-	# filter out weak detections by ensuring the confidence is
-	# greater than the threshhold confidence (20%)
-	if confidence > threshhold:
-		# extract the index of the class label from the detections,
-		# then compute the (x, y)-coordinates of the bounding box for
-		# the object
-		idx = int(detections[0, 0, i, 1])
-		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-		(startX, startY, endX, endY) = box.astype("int")
 
-		# display the prediction
-		label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-		print("[INFO] {}".format(label))
-		cv2.rectangle(image, (startX, startY), (endX, endY),
-			COLORS[idx], 2)
-		y = startY - 15 if startY - 15 > 15 else startY + 15
-		cv2.putText(image, label, (startX, y),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+    # filter out weak detections by ensuring the confidence is
+    # greater than the threshhold confidence (50%)
+    if confidence > threshhold:
+        # extract the index of the class label from the detections,
+        # then compute the (x, y)-coordinates of the bounding box for
+        # the object
+        idx = int(detections[0, 0, i, 1])
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
 
+        # display the prediction
+        label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+        print("[INFO] {}".format(label))
+        cv2.rectangle(image, (startX, startY), (endX, endY),
+                      COLORS[idx], 2)
+        y = startY - 15 if startY - 15 > 15 else startY + 15
+        cv2.putText(image, label, (startX, y),
+                    cv2.FONT_HERSHEY_TRIPLEX, 2, COLORS[idx], 2)
 
 # has a person been detected
 if 'person' in label:
-        print("yes")
-        desired_detect = True
-else:
-	desired_detect = False
+    print("yes")
+    desired_detect = True
+    import pygame as pg
 
-# show the output image
+    pg.mixer.init()
+    pg.mixer.music.load("belltone.mp3")  # file name here
+    pg.mixer.music.play()
+else:
+    desired_detect = False
+
+
+
+
+# resize output and show the output image
+image = cv2.resize(image, (0,0), fx=0.25, fy=0.25)
 cv2.imshow("Output", image)
-cv2.waitKey(3000)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
+
 
